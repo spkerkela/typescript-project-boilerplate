4c89539ac245d3311cfd82a7d6a771cc20ed0616
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2023-06-01 13:55:30 +0300

    update imports

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
-import { PlaywrightTestConfig, WebServerConfig } from "@playwright/test";
+import { PlaywrightTestConfig } from "@playwright/test";
 
 const localUrl = "http://localhost:3000";
 
-const webServer: WebServerConfig = {
+const webServer = {
   command: "npm run start",
   url: localUrl,
   reuseExistingServer: !process.env.CI,
